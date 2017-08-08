d96d9f56f250b57cb569fb65d9373211c63295dc
Ramsés Moreno <ramses@cuatromedios.com>
2017-08-08 11:34:18 -0500

    Working version
    Working version

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # Timecodes Remover
 
-Reads several .txt or .str files, removes timecodes and saves new files
+Reads several .txt or .str files, removes time codes from a file used as subtitles and saves new files. 
 
 ### Installation
 
 1. Install node if not yet installed in your computer (https://nodejs.org/en/)
 1. Change to the Timecodes Remover directory
-1. Put your .txt or .str files in procedded directory
+1. Put your .txt or .str files in `sources directory
 1. Run in command line `node removetimecodes`
 
 ### Example
 ##### Source file
-`00:00:20.125,00:00:21.417
- How many cows do I have?
- 00:00:21.417,00:00:23.542
- How many steps 
- to get to a place?`
+```text
+00:00:20.125,00:00:21.417
+How many cows do I have?
+`
+00:00:21.417,00:00:23.542
+How many steps 
+to get to a place?
+```
 
 ##### Processed file
-`How many cows do I have?
- How many steps 
- to get to a place?`
+```text
+How many cows do I have?
+How many steps to get to a place?
+```
--- removetimecodes.js
+++ removetimecodes.js
@@ -1,7 +1,32 @@
-const sourceFolder = './sources/';
-const destFolder = './processed/';
+const sourceFolder = '/sources/';
+const destFolder = '/processed/';
 const fs = require('fs');
+const path = require('path');
 
-fs.readdirSync(sourceFolder).forEach(file => {
-    console.log(file);
-})
+fs.readdir(path.join(__dirname+  sourceFolder),  (err, files) => {
+    if (err) {
+        throw err;
+    }
+    files.forEach(file => {
+        let fileParts = file.split(".");
+        let extension = fileParts[fileParts.length -1];
+        if (extension == 'srt' || extension == 'txt') {
+            fs.readFile(path.join(__dirname+  sourceFolder +  file), (err, content) => {
+                let fileContent = content.toString();
+                fileContent = fileContent.replace(/[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3},[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}/gi, "");
+                fileContent = fileContent.replace(/\.{3}/gi, "…");
+                fileContent = fileContent.replace(/\r\n|\r|\n/gi, " ");
+                fileContent = fileContent.replace(/\. /gi, ".\n\n");
+                fileContent = fileContent.replace(/\!/gi, "\!\n\n");
+                fileContent = fileContent.replace(/\?/gi, "\?\n\n");
+                fileContent = fileContent.replace(/\n {1,}/gi, "\n");
+                fileContent = fileContent.replace(/ {2,}/gi, " ");
+                fileContent = fileContent.replace(/^[ ]{1,}/gm, "");
+                //console.log(fileContent);
+                fs.writeFile(path.join(__dirname+  destFolder +  file), fileContent, (err) => {
+                    console.log(file, "done");
+                })
+            })
+        }
+    });
+});
